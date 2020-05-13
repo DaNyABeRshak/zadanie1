3bdb0582165236f16ae27387ff680b5571f9c354
DaNyABeRshak <danil.bershak@gmail.com>
2020-05-13 19:49:21 +0500

    Commit

---
+++ ConsoleApp28/ConsoleApp28/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+public class lib
+{
+    public static void Main(string[] args)
+    {
+        static  void pous()
+        {
+            Console.WriteLine("Hello World");
+
+        }
+        static  int sum(int a, int b)
+        {
+            return a + b;
+
+        }
+        static  string st1(string a)
+        {
+            return a;
+
+        }
+
+    }
+}
